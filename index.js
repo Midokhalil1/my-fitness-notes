c36da1a6ddf17021b65c5e6e9bce14bfd70397b5
Midokhalil1 <midokhalil22@gmail.com>
2022-11-22 10:20:05 -0500

    updatenote, getone, deletenote

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 import express from 'express';
-import { addNewNotes, getAllNotes, updateNotes } from './src/notes.js';
+import { addNewNote, getAllNotes, updateNote, getOneNote, deleteNote  } from './src/notes.js';
 
 
 // create a new server
 const app = express();
 app.use(express.json())
 
-app.post('/notes', addNewNotes)
+app.post('/notes', addNewNote)
 app.get('/notes', getAllNotes)
-app.patch('/notes', updateNotes)
+app.patch('/notes/:noteId', updateNote)
+app.get('/notes/:noteId',getOneNote)
+app.delete('/notes/:noteId', deleteNote)
 
-app.get('/', (req, res) => {
-    res.send('Hello')
-})
+
+// app.get('/', (req, res) => {
+//     res.send('Hello')
+// })
 // defining the port 
 const PORT = 3030;
 
--- src/notes.js
+++ src/notes.js
@@ -1,19 +1,20 @@
 import { dbConnect } from "./dbConnect.js"
+import { ObjectId } from "mongodb"
 
 // importing dbConnect 
 
 // add new exercise notes
 
-export async function addNewNotes(req, res) {
-    const newNotes = req.body
+export async function addNewNote(req, res) {
+    const newNote = req.body
     const db = dbConnect()
     await db.collection("notes").insertOne
-    (newNotes)
+    (newNote)
     .catch(err => {
         res.status(500).send(err)
         return 
 })
-res.status(201).send({ message: "New Notes Added." })
+res.status(201).send({ message: "New Note Added." })
 }
 
 export async function getAllNotes(req, res) {
@@ -22,12 +23,50 @@ export async function getAllNotes(req, res) {
     res.send(collection)
 }
 
-export async function findNotesByType(req,res) {
+// export async function findNotesByType(req,res) {
+//     const db = dbConnect()
+//     const { search } = req.params
+//     const collection = await db.collection("exercise notes").find({ type: search }).toArray()
+//     res.send(collection)
+// }
+
+export async function updateNote(req, res) {
+    const { noteId } = req.params
     const db = dbConnect()
-    const { search } = req.params
-    const collection = await db.collection("exercise notes").find({ type: search }).toArray()
+    await db.collection('notes')
+        .findOneAndUpdate({ _id: new ObjectId(noteId) }, { $set: req.body })
+        .catch(err => {
+            res.status(500).send(err)
+            return
+        })
+    res.status(202).send({ message: "note updated" })
+    getAllNotes(req, res)//?//
+}
+export async function getOneNote(req, res) {
+    const db = dbConnect()
+    const { noteId } = req.params
+    const collection = await db.collection("notes")
+        .findOne({ _id: new ObjectId(noteId) })
     res.send(collection)
 }
+
+export async function deleteNote(req, res) {
+    const db = dbConnect()
+    const { noteId } = req.params
+    const collection = await db.collection("notes")
+        .findOneAndDelete({ _id: new ObjectId(noteId) })
+    res.status(203).send('notes Deleted')
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
